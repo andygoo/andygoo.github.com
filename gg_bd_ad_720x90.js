4fd5a603af4efa7cbf11e0e54f1232b4b7adeba2
andygoo <hackoo7@qq.com>
2017-08-13 21:28:22 +0800

    ''

--- gg_bd_ad_720x90.js
+++ gg_bd_ad_720x90.js
@@ -1,3 +1,3 @@
-document.writeln('<div id="adsbygooglediv"');
+document.writeln('<div id="adsbygooglediv">');
 document.writeln('<script async src="//blog.jiesc.net/static/js/adsbygoogle.js"></script>');
 document.writeln("</div>");
