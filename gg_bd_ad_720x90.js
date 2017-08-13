33f14c02d2e5dbe410bb4a1616504b37cc20d66e
andygoo <hackoo7@qq.com>
2017-08-13 12:39:35 +0800

    ''

--- gg_bd_ad_720x90.js
+++ gg_bd_ad_720x90.js
@@ -1,4 +1,19 @@
 
-(function() {
-	
-}());
+/*
+document.writeln("<div style=\"width:970px;margin:10px auto;\">")
+document.writeln('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
+document.writeln('<!-- html5tricks-demo-big -->');
+document.writeln('<ins class="adsbygoogle"');
+document.writeln('     style="display:inline-block;width:970px;height:250px"');
+document.writeln('     data-ad-client="ca-pub-4188263447419139"');
+document.writeln('     data-ad-slot="5506515209"></ins>');
+document.writeln('<script>');
+document.writeln('(adsbygoogle = window.adsbygoogle || []).push({});');
+document.writeln('</script>');
+document.writeln("</div>");
+*/
+
+
+document.writeln("<div style=\"width:970px;margin:10px auto;\">");
+document.writeln("<img src=\"http://img.jiesc.net/2017/08/13/15025989873251.jpg\">");
+document.writeln("</div>");
