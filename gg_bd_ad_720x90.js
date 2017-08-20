1857b979d0bddfedd905b5af6ae70f5334fc8815
andygoo <hackoo7@qq.com>
2017-08-20 11:43:09 +0800

    ''

--- gg_bd_ad_720x90.js
+++ gg_bd_ad_720x90.js
@@ -8,5 +8,5 @@ document.writeln('<div id="adsbygooglediv">');
 document.writeln('<script type="text/javascript">');
 document.writeln('var cpro_id = "u3066572";');
 document.writeln('</script>');
-document.writeln('<script type="text/javascript" src="http://cpro.baidustatic.com/cpro/ui/c.js"></script>');
+document.writeln('<script async src="http://cpro.baidustatic.com/cpro/ui/c.js"></script>');
 document.writeln("</div>");
