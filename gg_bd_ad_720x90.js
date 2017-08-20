1aaa63160e1c96da5711f0ae9d8335ffb79b0403
andygoo <hackoo7@qq.com>
2017-08-20 12:01:16 +0800

    ''

--- gg_bd_ad_720x90.js
+++ gg_bd_ad_720x90.js
@@ -3,10 +3,26 @@ document.writeln('<div id="adsbygooglediv">');
 document.writeln('<script async src="//blog.jiesc.net/static/js/adsbygoogle.js"></script>');
 document.writeln("</div>");
 */
-
+function isMobile() {
+    var ua = navigator.userAgent.toLowerCase();
+    if (ua.match(/(android|iphone)/i)){
+		return true;
+    }
+	return false;
+}
+var _isMobile = isMobile();
+if (_isMobile) {
+document.writeln('<div id="adsbygooglediv">');
+document.writeln('<script type="text/javascript">');
+document.writeln('var cpro_id = "u3066579";');
+document.writeln('</script>');
+document.writeln('<script type="text/javascript" src="http://cpro.baidustatic.com/cpro/ui/cm.js"></script>');
+document.writeln("</div>");
+} else {
 document.writeln('<div id="adsbygooglediv">');
 document.writeln('<script type="text/javascript">');
 document.writeln('var cpro_id = "u3066572";');
 document.writeln('</script>');
 document.writeln('<script async src="http://cpro.baidustatic.com/cpro/ui/c.js"></script>');
 document.writeln("</div>");
+}
