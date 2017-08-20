0901bb9f66add0188911fbc410df86ec13add8e2
andygoo <hackoo7@qq.com>
2017-08-20 11:40:14 +0800

    ''

--- gg_bd_ad_720x90.js
+++ gg_bd_ad_720x90.js
@@ -1,3 +1,12 @@
+/*
 document.writeln('<div id="adsbygooglediv">');
 document.writeln('<script async src="//blog.jiesc.net/static/js/adsbygoogle.js"></script>');
+document.writeln("</div>");
+*/
+
+document.writeln('<div id="adsbygooglediv">');
+document.writeln('<script type="text/javascript">');
+document.writeln('var cpro_id = "u3066572";');
+document.writeln('</script>');
+document.writeln('<script type="text/javascript" src="http://cpro.baidustatic.com/cpro/ui/c.js"></script>');
 document.writeln("</div>");
